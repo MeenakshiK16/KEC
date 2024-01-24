1a4c9ce48ede8192356d207639ff3e18ad19a46d
MeenakshiK16 <meenakshik.21eie@kongu.edu>
2024-01-24 10:22:33 +0530

    version-1

---
+++ react-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hello from './Hello'
+import Ter from './Ter'
+export default function App(){
+    return(
+        <div>
+            <Ter/>
+        </div>
+    )
+}
